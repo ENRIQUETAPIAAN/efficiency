1aa3ac3fdc638c00ff9fac17a1f6a58f6008bb0c
enriquetapiaan <tapiaa.antunes@outlook.com>
2024-08-26 18:17:19 -0600

    seccion para agregar reportes, terminada.

--- agregar-reporte.php
+++ agregar-reporte.php
@@ -2,7 +2,74 @@
 
 if(isset($_SESSION['usuario'])){
     require 'admin/config.php';
-    require 'views/agregar-reporte.view.php';   
+    require 'functions.php';
+
+    $conexion = conexion_reportes($db_config_reportes);
+    if(!$conexion){
+        header('Location: error.php');
+    }
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $p_programada = limpiarDatos($_POST['p_programada']);
+        $p_real = limpiarDatos($_POST['p_real']);
+        $celula = $_POST['celula'];
+        $turno = $_POST['turno'];
+        $mano_de_obra = $_POST['mano_de_obra'];
+        if(empty($mano_de_obra)){
+            $mano_de_obra = 0;
+        }
+        $maquinaria = $_POST['maquinaria'];
+        if(empty($maquinaria)){
+            $maquinaria = 0;
+        }
+        $material = $_POST['material'];
+        if(empty($material)){
+            $material = 0;
+        }
+        $metodo = $_POST['metodo'];
+        if(empty($metodo)){
+            $metodo = 0;
+        }
+        $medicion = $_POST['medicion'];
+        if(empty($medicion)){
+            $medicion = 0;
+        }
+        $madre_natur = $_POST['madre_natur'];
+        if(empty($madre_natur)){
+            $madre_natur = 0;
+        }
+        $comentarios = limpiarDatos($_POST['comentarios']);
+        
+        $foto = $_FILES['thumb']['tmp_name'];
+
+        $archivo_subido = $blog_config['carpeta_imagenes'].$_FILES['thumb']['name'];
+
+        move_uploaded_file($foto, $archivo_subido);
+
+        $nombre_usuario = $_SESSION['usuario'];
+
+        $statement = $conexion->prepare('INSERT INTO tb_reportes (id_reporte, nombre, p_real, p_programada, celula, turno, mano_de_obra,
+        maquinaria, material, metodo, medicion, madre_natur, comentarios, foto) VALUES (null, :nombre, :p_real, :p_programada, :celula, 
+        :turno, :mano_de_obra, :maquinaria, :material, :metodo, :medicion, :madre_natur, :comentarios, :foto)');
+
+        $statement->execute(array(':nombre' => $nombre_usuario, 
+        ':p_real' => $p_real, 
+        ':p_programada' => $p_programada, 
+        ':celula' => $celula, 
+        ':turno' => $turno, 
+        ':mano_de_obra' => $mano_de_obra,
+        ':maquinaria' => $maquinaria, 
+        ':material' => $material, 
+        ':metodo' => $metodo, 
+        ':medicion' => $medicion, 
+        ':madre_natur' => $madre_natur, 
+        ':comentarios' => $comentarios, 
+        ':foto' => $_FILES['thumb']['name']));
+
+        header('Location: index.php');
+    }
+
+    require 'views/agregar-reporte.view.php';
 } else{
     header('Location: index.php');
 }   
--- views/agregar-reporte.view.php
+++ views/agregar-reporte.view.php
@@ -2,35 +2,40 @@
 <?php require 'header.php'?>
         <section>
             <div class="cont-form">
-                <form action="" name="login">
+                <form enctype="multipart/form-data" method="post" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" name="login">
                     <div class="cont-imagen">
                         <img src="img/subir2.png" alt="">
-                        <input type="file" name="" id="">
+                        <input type="file" name="thumb">
                     </div>
                     <div class="cont-input margin-bottom">
                         <div class="cont-input-number">
                             <label for="">Programado</label>
-                            <input type="number" name="" id="" placeholder="">
+                            <input type="number" name="p_programada" id="" placeholder="">
                         </div>
                         <div class="cont-input-number">
                             <label for="">Real</label>
-                            <input type="number" name="" id="" placeholder="">
+                            <input type="number" name="p_real" id="" placeholder="">
                         </div>
                     </div>
                     <div class="cont-6m">
                         <div class="cont-input">
                             <div class="cont-input-number">
                                 <label for="">Celula</label>
-                                <select name="" id="">
+                                <select name="celula" id="">
                                     <option disabled selected>Selecciona celula</option>
                                     <option value="112 A">112 A</option>
                                     <option value="112 B">112 B</option>
                                     <option value="112 C">112 C</option>
+                                    <option value="112 D">112 D</option>
+                                    <option value="112 E">112 E</option>
+                                    <option value="112 F">112 F</option>
+                                    <option value="112 G">112 G</option>
+                                    <option value="112 H">112 H</option>
                                 </select>
                             </div>
                             <div class="cont-input-number">
                                 <label for="">Turno</label>
-                                <select name="" id="">
+                                <select name="turno" id="">
                                     <option disabled selected>Selecciona turno</option>
                                     <option value="Turno 1">Turno 1</option>
                                     <option value="Turno 2">Turno 2</option>
@@ -41,38 +46,38 @@
                         <div class="cont-input">
                             <div class="cont-input-number">
                                 <label for="">Mano de obra</label>
-                                <input type="number" name="" id="" placeholder="">
+                                <input type="number" name="mano_de_obra" id="" placeholder="">
                             </div>
                             <div class="cont-input-number">
                                 <label for="">Maquinaria</label>
-                                <input type="number" name="" id="" placeholder="">
+                                <input type="number" name="maquinaria" id="" placeholder="">
                             </div>
                         </div>
                         <div class="cont-input">
                             <div class="cont-input-number">
                                 <label for="">Material</label>
-                                <input type="number" name="" id="" placeholder="">
+                                <input type="number" name="material" id="" placeholder="">
                             </div>
                             <div class="cont-input-number">
                                 <label for="">Metodo</label>
-                                <input type="number" name="" id="" placeholder="">
+                                <input type="number" name="metodo" id="" placeholder="">
                             </div>
                         </div>
                         <div class="cont-input">
                             <div class="cont-input-number">
                                 <label for="">Medición</label>
-                                <input type="number" name="" id="" placeholder="">
+                                <input type="number" name="medicion" id="" placeholder="">
                             </div>
                             <div class="cont-input-number">
                                 <label for="">Madre Natur.</label>
-                                <input type="number" name="" id="" placeholder="">
+                                <input type="number" name="madre_natur" id="" placeholder="">
                             </div>
                         </div>
                     </div>
                     
                     <div class="cont-input-textarea">
                         <label for="">Comentarios</label>
-                        <textarea name="" id=""></textarea>
+                        <textarea name="comentarios" id=""></textarea>
                     </div>
                     <div class="btn">
                         <a href="index.html">Volver</a>
